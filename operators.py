e4a8b9fdeaa25d434c9b87b14e829ee9b47681af
Eduardo Davila <72955250+Erickvoid@users.noreply.github.com>
2023-04-24 20:24:48 -0600

    Add operators

---
+++ operators.py
@@ -0,0 +1,21 @@
+addition = 10 + 5 #Dara como resultado 15
+substraction = 10 - 5 # Dara como resultado 5
+multiplication = 10 * 5 # Dara como resultado 50
+
+# Comparison 
+age = 10 == 5 #False
+age = 10 != 5 #True
+age = 10  > 5 #True
+
+# Logical operation
+age_boy = 10
+age_girl = 14
+
+# Assignment Operation
+
+age_boy > 8 & age_girl > 8 #True 
+
+age = 10 
+age += 10 
+age -= 10 
+age *= 10 
